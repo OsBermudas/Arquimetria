91ac56d460bbb798cf4a30bc239754f506d2e2e5
The Henrique <rique.souza06@icloud.com>
2025-10-09 18:49:03 -0300

    Configurações de arquivos

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <allegro5/allegro.h>
 
 int main() {
-	printf("Hello, World!\n");
-	return 0;
+	ALLEGRO_DISPLAY* janela = NULL;
+	al_init();
+	janela = al_create_display(640, 480);
+    al_clear_to_color(al_map_rgb(255, 255, 255));
+    al_flip_display();
+    al_rest(10.0);
+    al_destroy_display(janela);
 }
